b68b609e5d49954db8abb4d0e3733e8628f9737f
RainnWorks release <releases@rainn.works>
2026-09-24 16:31:17 +0000

    pinch 1.0.3

--- Casks/pinch.rb
+++ Casks/pinch.rb
@@ -1,6 +1,6 @@
 cask "pinch" do
-  version "1.0.2"
-  sha256 "0f3f1398ee83ab68122a49cd999cc0130706e7951797e0576e6a147135595f62"
+  version "1.0.3"
+  sha256 "a231b1b8d384e660a646a18f14cca00bdfc386443a2667c84b6469390ac8cdbe"
 
   url "https://github.com/RainnWorks/pinch/releases/download/v#{version}/Pinch-#{version}.dmg"
   name "Pinch"
